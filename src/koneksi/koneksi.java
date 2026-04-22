f5ab64981deb48cb214752013f9e78d03ecccb86
faisa <faisa@Adam>
2026-04-22 14:33:56 +0700

    hehebuayyy-app_P

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+import java.sql.*;
+/**
+ *
+ * @author faisa
+ */
+public class koneksi {
+    private Connection koneksi;
+    public Connection connect(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("berhasil konek");
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("gagal konek"+ex);
+        }
+        String url = "jdbc:mysql://localhost:3307/penjualan";
+        try{
+            koneksi = DriverManager.getConnection(url,"root","");
+            System.out.println("berhasil konek ke database");
+        }
+        catch(SQLException ex){
+            System.out.println("gagal konek ke database"+ex);
+        }
+        return koneksi;
+    }
+}
